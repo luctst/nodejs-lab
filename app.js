8d79afff52157dfc451af273590dbdd61d154615
luctst <luc.tostee@outlook.fr>
2018-09-09 17:22:04 +0200

    Add yargs package and how to manipulate data from the command line

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
+/**
+* Node modules 
+*/
 const Fs = require("fs");
-const Os = require("os");
-const Note = require("./notes.js"); // If there is no module.exports inside the file require launch directly the instructions inside him.
 const _ = require("lodash"); // With require NodeJs first look in your core application if you have module named lodash if not nodeJs look in the node_modules folder and if still not node look in the nodeJs folder install on your system.
-console.log(_.isString(false));
+const Yargs = require("yargs");
 
-let user = Os.userInfo();
-let res = Note.addNote;
+/**
+ * Own modules
+ */
+const Note = require("./notes.js"); // Retrieve the data passed with module.exports in an object Note, and if there is some code outside module.exports launch the code.
 
-// console.log(`Hello mon nom est ${user.username} et j'ai ${Note.age}`);
-// console.log(res);
+/**
+ * Variables
+ */
+const Name = "Lucas";
+const Process = process.argv[2]; // Array who contains arguments lists passed by the command line.
+const YargsArgv = Yargs.argv;
+// let res = Note.addNote;
+
+/**
+ * Execution
+ */
+console.log(" argv property in Process object in global node object:", process.argv);
+console.log(`Yargs object:`,YargsArgv);
+switch (Process) {
+    case "add":
+        Note.AddNote(YargsArgv.title, YargsArgv.body);
+        break;
+    case "list":
+        Note.ListAllNote();
+        break;
+    case "read":
+        Note.ReadNote(YargsArgv.title);
+        break;
+    default:
+        console.log("Command not recognize");
+        break;
+}
+// console.log(_.isString(false));
+// console.log(`Hello mon nom est ${Name} et j'ai ${Note.age} ans`);
+// console.log(res());
 // console.log(`the result is ${Note.addNumber(2,2)}`); // You can directly call your method of your object because remember module.exports export the data in an object here Note.
-// console.log(Note);
 
 // Fs.appendFile("greeting.txt", `Hello ${user.username}`, error => { // function allows to create file
 //     if (error) {
--- notes.js
+++ notes.js
@@ -1,12 +1,30 @@
-console.log("starting notes.js file");
+// console.log("starting notes.js file");
 
-module.exports.age = 23; // Dés que l'on va utiliser un require pour appeler ce fichier un object avec le nom indiqué sera crée et aura comme propriété ce qui est exporté avec module.
+// module.exports.age = 23; // Dés que l'on va utiliser un require pour appeler ce fichier un object avec le nom indiqué sera crée et aura comme propriété ce qui est exporté avec module.
 
-module.exports.addNote = () => {
-    console.log("addNote");
-    return "New note";
+// module.exports.addNote = () => { // Pareille pour les fonctions
+//     console.log("addNote");
+//     return "New note";
+// };
+
+// module.exports.addNumber = (a,b) => {
+//     return a + b;
+// };
+
+const AddNote = (title, body) => {
+    console.log("Adding a new note:", title, body);
 };
 
-module.exports.addNumber = (a,b) => {
-    return a + b;
+const ListAllNote = () => {
+    console.log("Get all notes");
 };
+
+const ReadNote = (title) => {
+    console.log(`Content of the note with the title '${title}':`);
+};
+
+module.exports = {
+    AddNote, // ES6 feature when property = name you can write like this.
+    ListAllNote,
+    ReadNote,
+}
