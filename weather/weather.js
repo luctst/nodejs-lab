4d1138bfe29dd62106c6c69db440e3bdae59ce26
luctst <luc.tostee@outlook.fr>
2018-10-04 15:37:28 +0200

    Add axios package

--- weather/weather.js
+++ weather/weather.js
@@ -2,23 +2,27 @@
  * Import
  */
 const fetch = require("node-fetch");
+const Yargv = require("yargs");
+
+/**
+ * Variables
+ */
+
+ /**
+  * Déclaration
+  */
+ const getApi = async () => {
+     try {
+         let data = await fetch(`https://www.metaweather.com/api/location/2487956/`);
+         const result =  await data.json();
+         console.log(`I'm waiting for an await function`);
+         console.log(result.parent);
+     } catch (error) {
+         console.log(error);
+     }
+ };
 
 /**
  * Execution
  */
-const getApi = async () => {
-    try {
-        let data = await fetch(`https://www.metaweather.com/api/location/2487956/`);
-        const result =  await data.json();
-        console.log(`I'm waiting for an await function`);
-        console.log(result.parent);
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-const test = async () => {
-    console.log(`bonjour`);
-};
-
 getApi();
