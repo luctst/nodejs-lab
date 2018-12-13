4072cd0f1626b46958ad476ef9752c40986c8ef5
luctst <luc.tostee@outlook.fr>
2018-12-13 19:54:25 +0100

    Edit code

--- weather-app/server.js
+++ weather-app/server.js
@@ -5,15 +5,22 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 const fetch = require("node-fetch");
+const queryString = require("querystring");
 
 /**
  * Exécution
  */
 http.createServer((req, res) => {
-    let urlPath = url.parse(req.url).pathname;
-    // console.log(url.parse(req.url));
+    let { pathname } = url.parse(req.url);
+    let body;
 
-    if (urlPath === "/") {
+    
+    if (pathname === "/") {
+        if (req.method === "POST") {
+            req.on("data", data => {
+                body = queryString.parse(data.toString());
+            });
+        }
         fs.readFile(`${__dirname}/src/index.html`, (error, data) => {
             if (error) {
                 throw error;
@@ -22,7 +29,7 @@ http.createServer((req, res) => {
                 res.end();
             }
         });
-    } else if (urlPath === "/app.js") {
+    } else if (pathname === "/app.js") {
         fs.readFile(`${__dirname}/src/app.js`, (error, data) => {
             if (error) {
                 throw error;
@@ -31,5 +38,9 @@ http.createServer((req, res) => {
                 res.end();
             }
         });
+    } else if (pathname === "/test") {
+        req.on("data", chunk => console.log(`Data is arrived: ${chunk}`));
+        res.write("Test page");
+        res.end();
     }
 }).listen(3000, "127.0.0.1");
