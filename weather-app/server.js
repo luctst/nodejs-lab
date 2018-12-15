9f0f6d98be9e49e6c083cb2026f7f08684c08e5e
luctst <luc.tostee@outlook.fr>
2018-12-15 13:33:12 +0100

    Retrieve weather data

--- weather-app/server.js
+++ weather-app/server.js
@@ -4,22 +4,43 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const fetch = require("node-fetch");
 const queryString = require("querystring");
+const apiId = `26e8fc76ea4289676e61e4f91583579d`;
+
+
+/**
+ * Déclaration
+ */
+const getData = async (query, retrieveData, response) => {
+    let data;
+    try {
+        query = retrieveData;
+        data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&APPID=${apiId}`);
+        let json = await data.json();
+        json = JSON.stringify(json);
+        response.write(json);
+        response.end();
+    } catch (error) {
+        throw error;
+    }
+}
 
 /**
  * Exécution
  */
 http.createServer((req, res) => {
     let { pathname } = url.parse(req.url);
-    let body;
+    let bodyParsed;
+    let query = "";
     
     if (pathname === "/") {
         if (req.method === "POST") {
             req.on('data', chunk => {
-                body += chunk.toString();
-                console.log(queryString.parse(body));
-                res.end(body);
-            });
+                bodyParsed = queryString.parse(chunk.toString());
+            }).on("end", () => {
+                getData(query, bodyParsed.city, res);
+            })
         } else {
             fs.readFile(`${__dirname}/src/index.html`, (error, data) => {
                 error ? console.log(error) : res.end(data);
