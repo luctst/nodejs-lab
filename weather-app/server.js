c8e9ae9f37201887c1dd908d37b21ea79a7f1a80
luctst <luc.tostee@outlook.fr>
2018-12-17 02:45:34 +0100

    Add weather template

--- weather-app/server.js
+++ weather-app/server.js
@@ -7,6 +7,8 @@ const url = require("url");
 const fetch = require("node-fetch");
 const queryString = require("querystring");
 const apiId = `26e8fc76ea4289676e61e4f91583579d`;
+const path = require("path");
+const day = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
 
 
 /**
@@ -14,13 +16,25 @@ const apiId = `26e8fc76ea4289676e61e4f91583579d`;
  */
 const getData = async (query, retrieveData, response) => {
     let data;
+    let dataReplace;
+
     try {
         query = retrieveData;
         data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=${apiId}`);
         let json = await data.json();
-        json = JSON.stringify(json);
-        let dataRes = JSON.parse(json);
-        response.write(json);
+
+        fs.readFile(path.resolve(__dirname, "src", "js", "view", "weather.html"), "utf-8", (error, content) => {
+            if (error) throw error;
+            dataReplace = content.replace(/\{%city%}/g, json.name);
+            dataReplace = dataReplace.replace(/{%date%}/g, day[new Date().getDay() - 1]);
+            dataReplace = dataReplace.replace(/{%temp%}/, json.main.temp);
+            dataReplace = dataReplace.replace(/{%tempMax%}/, json.main.temp_max);
+            dataReplace = dataReplace.replace(/{%tempMin%}/, json.main.temp_min);
+            dataReplace = dataReplace.replace(/{%humidity%}/, json.main.humidity);
+            dataReplace = dataReplace.replace(/{%speed%}/, json.wind.speed);
+            response.write(dataReplace);
+            response.end();
+        });
     } catch (error) {
         throw error;
     }
@@ -42,18 +56,18 @@ http.createServer((req, res) => {
                 getData(query, bodyParsed.city, res);
             })
         } else {
-            fs.readFile(`${__dirname}/src/index.html`, (error, data) => {
+            fs.readFile(path.resolve(__dirname, "src", "index.html"), (error, data) => {
                 error ? console.log(error) : res.end(data);
             });
         }
     } else if (pathname === "/app.js") {
-        fs.readFile(`${__dirname}/src/app.js`, (error, data) => {
+        fs.readFile(path.resolve(__dirname, "src", "app.js"), (error, data) => {
             if (error) throw error;
             res.write(data);
             res.end();
         });
     } else if (pathname === "/main.css") {
-        fs.readFile(`${__dirname}/src/main.css`, (error, data) => {
+        fs.readFile(path.resolve(__dirname, "src", "main.css"), (error, data) => {
             if (error) throw error;
             res.write(data);
             res.end();
