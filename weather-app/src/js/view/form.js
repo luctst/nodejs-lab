5283997412ab06fabf5e888129c36069ed24e634
luctst <luc.tostee@outlook.fr>
2018-12-17 02:46:17 +0100

    Start render short weather information

--- weather-app/src/js/view/form.js
+++ weather-app/src/js/view/form.js
@@ -6,38 +6,54 @@ import React from "react";
 /**
  * Déclaration
  */
-export default class Form extends React.Component {
-    state = {
-        showData: false,
-        weatherData: {},
+export class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: "",
+            showData: false,
+            weatherData: {},
+        }
     }
     displayData = () => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.query}&units=metric&APPID=26e8fc76ea4289676e61e4f91583579d`)
+            .then(response => response.json()
+                .then(data => {
+                    this.setState({ weatherData: data });
+                }));
         let button = this.state.showData;
         this.setState({
-            showData: !button
+            showData: !button,
         });
     }
-    componentDidMount() {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=Bordeaux&units=metric&APPID=26e8fc76ea4289676e61e4f91583579d`)
-            .then(response => response.json())
-            .then(data => this.setState({ weatherData: data }))
-            .catch(error => error);
+    getQuery = event => {
+        this.setState({
+            query: event.target.value
+        });
     }
     render() {
         return (
             <section className="row" style={{margin:"10% 0"}}>
-                <form action="/" method="POST">
+                <form action="/weather.html" method="POST">
                     <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Enter your city" name="city"></input>
+                        <input type="text" className="form-control" placeholder="Enter your city" name="city" onChange={this.getQuery}></input>
                     </div>
                     <div className="form-group">
-                        <input className="btn btn-warning" value="Get weather" onClick={this.displayData} ></input>
+                        <input className="btn btn-warning" value="Get weather" onClick={this.displayData}></input>
                         <input type="submit" className="btn btn-primary" value="get more info" style={{ marginLeft: "10px" }}></input>
                     </div>
                 </form>
                 {
                     this.state.showData ?
-                        <p>{this.state.weatherData.name}</p>
+                        <div>
+                            <h3>Weather in {this.state.weatherData.name} :</h3>
+                            {/* <ul className="list-group">
+                                <li className="list-group-item">The temperature is about {this.state.weatherData.main.temp} degrees.</li>
+                                <li className="list-group-item">The max temperature will be {this.state.weatherData.main.temp_max}</li>
+                                <li className="list-group-item">The min temperature will be {this.state.weatherData.main.temp_min}</li>
+                                <li className="list-group-item">The humidity will be of {this.state.weatherData.main.humidity} %</li>
+                            </ul> */}
+                        </div>
                     : null
                 }
             </section>
