0972094e83a4000717496cc8725d2a70beb4e0aa
luctst <luc.tostee@outlook.fr>
2018-12-17 02:47:54 +0100

    Deleted old weather folder

--- weather/async-basics.js
+++
@@ -1,8 +0,0 @@
-console.log("Premiére instruction");
-setTimeout( () => {
-    console.log("Asynchronous code");
-}, 2000);
-setTimeout(() => {
-    console.log("Another asynchronous code");
-}, 1000);
-console.log("Deuxiéme instruction");
