a7da271a8ff3da2f695140e1877851795251b3dd
luctst <luc.tostee@outlook.fr>
2018-12-19 01:24:31 +0100

    Edit view

--- weather-app/src/js/view/footer.js
+++ weather-app/src/js/view/footer.js
@@ -10,7 +10,7 @@ export const Footer = props => {
     return (
         <footer className="row">
             <div className="col-12">
-                <p className="small text-center"><a href={props.link} target="_blank">This is me !!</a></p>
+                <p className="small"><a href={props.link} target="_blank">Made by me :)</a></p>
             </div>
         </footer>
     );
--- weather-app/src/js/view/form.js
+++ weather-app/src/js/view/form.js
@@ -10,7 +10,7 @@ export class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            query: "",
+            query: null,
             showData: false,
             weatherData: {},
         }
@@ -18,9 +18,7 @@ export class Form extends React.Component {
     displayData = () => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.query}&units=metric&APPID=26e8fc76ea4289676e61e4f91583579d`)
             .then(response => response.json()
-                .then(data => {
-                    this.setState({ weatherData: data });
-                }));
+            .then(data => this.setState({ weatherData: data, mainData: Object.values(data.main)})));
         let button = this.state.showData;
         this.setState({
             showData: !button,
@@ -31,12 +29,12 @@ export class Form extends React.Component {
             query: event.target.value
         });
     }
-    render() {
+    render = () => {
         return (
-            <section className="row" style={{margin:"10% 0"}}>
-                <form action="/weather.html" method="POST">
+            <section className="row" style={{margin:"5% 0 0"}}>
+                <form action="/message" method="POST">
                     <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Enter your city" name="city" onChange={this.getQuery}></input>
+                        <input type="text" className="form-control" placeholder="Enter your city in english for now" name="city" onChange={this.getQuery}></input>
                     </div>
                     <div className="form-group">
                         <input className="btn btn-warning" value="Get weather" onClick={this.displayData}></input>
@@ -44,15 +42,15 @@ export class Form extends React.Component {
                     </div>
                 </form>
                 {
-                    this.state.showData ?
+                    this.state.mainData ?
                         <div>
                             <h3>Weather in {this.state.weatherData.name} :</h3>
-                            {/* <ul className="list-group">
-                                <li className="list-group-item">The temperature is about {this.state.weatherData.main.temp} degrees.</li>
-                                <li className="list-group-item">The max temperature will be {this.state.weatherData.main.temp_max}</li>
-                                <li className="list-group-item">The min temperature will be {this.state.weatherData.main.temp_min}</li>
-                                <li className="list-group-item">The humidity will be of {this.state.weatherData.main.humidity} %</li>
-                            </ul> */}
+                            <ul className="list-group">
+                                <li className="list-group-item">The temperature is about <strong>{this.state.weatherData.main.temp}</strong> degrees.</li>
+                                <li className="list-group-item">The max temperature will be <strong>{this.state.weatherData.main.temp_max}</strong> degrees</li>
+                                <li className="list-group-item">The min temperature will be <strong>{this.state.weatherData.main.temp_min}</strong> degrees</li>
+                                <li className="list-group-item">The humidity will be of <strong>{this.state.weatherData.main.humidity}</strong> %</li>
+                            </ul>
                         </div>
                     : null
                 }
