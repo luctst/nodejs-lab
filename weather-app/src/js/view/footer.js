b3aa208c665eeb42f4e3b5e3b696fea92e2374c9
luctst <luc.tostee@outlook.fr>
2018-12-23 13:00:24 +0100

    Update component

--- weather-app/src/js/view/footer.js
+++ weather-app/src/js/view/footer.js
@@ -9,9 +9,7 @@ import React from "react";
 export const Footer = props => {
     return (
         <footer className="row">
-            <div className="col-12">
-                <p className="small"><a href={props.link} target="_blank">Made by me :)</a></p>
-            </div>
+            <p className="small" style={{textAlign:"center"}}><a href={props.link} target="_blank">Made by me :)</a></p>
         </footer>
     );
 };
